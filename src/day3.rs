ddbaa00c29ec7b92973edad4ecc8236c9e03c1cc
Max Fischer <maxfischer2781@gmail.com>
2020-12-05 21:55:25 +0100

    solution for day 3 2

--- src/day3.rs
+++ src/day3.rs
@@ -6,14 +6,19 @@ use crate::utility::parse_file_lines;
 
 pub fn solve() -> Result<(), Error> {
     let map: Vec<MapLine> = parse_file_lines("data/day3.txt")?;
-    println!("Hits {}", sum_slope(map, 3, 1));
+    println!("Hits simple {}", sum_slope(&map, 3, 1));
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let slope_hits = slopes.iter().map(
+        |(r, d)| sum_slope(&map, *r, *d)
+    ).product::<i64>();
+    println!("Hits many {}", slope_hits);
     Ok(())
 }
 
-fn sum_slope(map: Vec<MapLine>, right: usize, down: usize) -> i32 {
+fn sum_slope(map: &Vec<MapLine>, right: usize, down: usize) -> i64 {
     let mut hits = 0;
     for (index, line) in map.iter().enumerate() {
-        match (line.is_tree(index * right), index % down) {
+        match (line.is_tree(index / down * right), index % down) {
             (true, 0) => hits += 1,
             _ => (),
         }
@@ -26,9 +31,11 @@ fn sum_slope(map: Vec<MapLine>, right: usize, down: usize) -> i32 {
 struct MapLine(Vec<bool>);
 
 impl MapLine {
+    /// Whether there is a tree at `i`
     fn is_tree(&self, i: usize) -> bool {
         self.0[i % self.0.len()]
-    }}
+    }
+}
 
 impl fmt::Display for MapLine {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -40,6 +47,7 @@ impl fmt::Display for MapLine {
 impl FromStr for MapLine {
     type Err = ();
 
+    /// Read a literal such as `.........#....#.###.........##.`
     fn from_str(s: &str)  -> Result<Self, Self::Err> {
         Ok(MapLine(
             s.chars().map(|c| c == '#').collect()
